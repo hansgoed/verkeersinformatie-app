33284428614da71fe15f27b87998ef522c49fc5e
hans <hansgoed@gmail.com>
2020-04-25 12:32:12 +0200

    [feat] Added support for active tabs & executing actions when a tab is selected.

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,25 @@
     <b-card title="Verkeersinformatie" no-body>
       <b-card-header header-tag="nav">
         <b-nav card-header tabs>
-          <b-nav-item active>Wegen</b-nav-item>
-          <b-nav-item>Files</b-nav-item>
-          <b-nav-item>Werkzaamheden</b-nav-item>
-          <b-nav-item disabled>Flitsers</b-nav-item>
+          <b-nav-item
+            v-for="title in links"
+            v-bind:key="title"
+            v-bind:active="selectedTab === title"
+            v-on:click="selectTab(title)"
+          >
+            {{ title }}
+          </b-nav-item>
+
+          <b-nav-item
+            disabled
+          >
+            Flitsers
+          </b-nav-item>
         </b-nav>
       </b-card-header>
       <b-card-body>
         <b-card-text>
-          <Roadlist />
+          <MenuContent v-bind:selectedTab="selectedTab"/>
         </b-card-text>
       </b-card-body>
     </b-card>
@@ -21,12 +31,27 @@
 <script>
 import './assets/overall_style.scss';
 
-import Roadlist from "./components/RoadList";
+import MenuContent from "./components/MenuContent";
 
 export default {
   name: 'App',
   components: {
-    Roadlist
+    MenuContent: MenuContent
+  },
+  data() {
+    return {
+      selectedTab: 'Wegen',
+      links: [
+        'Wegen',
+        'Files',
+        'Werkzaamheden'
+      ]
+    }
+  },
+  methods: {
+    selectTab: function (tab) {
+      this.selectedTab = tab;
+    }
   }
 }
 </script>
---
+++ src/components/MenuContent.vue
@@ -0,0 +1,40 @@
+<template>
+    <span>
+        <span
+            id="roadsOverview"
+            v-if="selectedTab === 'Wegen'"
+        >
+            <RoadsOverview />
+        </span>
+        <span
+            id="trafficJamsOverview"
+            v-if="selectedTab === 'Files'"
+        >
+            files
+        </span>
+        <span
+            id="roadworksOverview"
+            v-if="selectedTab === 'Werkzaamheden'"
+        >
+            werkzaamheden
+        </span>
+    </span>
+</template>
+<script>
+    import { mapState } from 'vuex'
+    import RoadsOverview from "./MenuContent/RoadsOverview";
+
+    export default {
+        components: {RoadsOverview},
+        props: {
+            "selectedTab": String
+        },
+        computed: mapState({
+            trafficJams: function (state) { return state.trafficJams.all },
+            roadworks: function (state) { return state.roadworks.all }
+        }),
+        created() {
+            this.$store.dispatch('roads/getAllRoads')
+        }
+    }
+</script>
